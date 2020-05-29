714d587c773874543ce0c92793ceb95dd7087f04
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2020-05-29 17:46:07 -0400

    feat: metrics tooltip, build(deps): upgrade dependencies (#200)

--- src/components/Routes/components/Course/components/Metrics/Metrics.tsx
+++ src/components/Routes/components/Course/components/Metrics/Metrics.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import round from '../../../../../../utils/round';
 import { ICourse } from '../../../../../../data/interfaces';
@@ -18,22 +19,26 @@ const Metrics: React.FC<IProps> = ({ course: { metric } }) => {
     return null;
   }
 
-  const data: { label: string; value: number }[] = [
+  const data: { label: string; title: string; value: number }[] = [
     {
       label: 'Reviews',
       value: metric.reviews.count,
+      title: 'Number of reviews',
     },
     {
       label: 'Avg. Difficulty',
       value: metric.reviews.difficulty.mean,
+      title: '1-Very Easy, 5-Very Hard',
     },
     {
       label: 'Avg. Workload',
       value: metric.reviews.workload.mean,
+      title: 'Hours per week',
     },
     {
       label: 'Avg. Rating',
       value: metric.reviews.rating.mean,
+      title: '1-Strongly Disliked, 5-Strongly Liked',
     },
   ];
 
@@ -41,12 +46,16 @@ const Metrics: React.FC<IProps> = ({ course: { metric } }) => {
     <Card className={classes.card}>
       <CardContent className={classes.cardContent}>
         <Grid container>
-          {data.map(({ label, value }) => (
+          {data.map(({ label, value, title }) => (
             <Grid item xs={12} md={3} key={label} className={classes.metric}>
-              <Typography variant="body2" color="textSecondary">
-                {label}
-              </Typography>
-              <Typography variant="subtitle2">{round(value, 2)}</Typography>
+              <Tooltip title={title} placement="top">
+                <div>
+                  <Typography variant="body2" color="textSecondary">
+                    {label}
+                  </Typography>
+                  <Typography variant="subtitle2">{round(value, 2)}</Typography>
+                </div>
+              </Tooltip>
             </Grid>
           ))}
         </Grid>
