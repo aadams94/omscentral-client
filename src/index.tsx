58476e73bb50de7f689b2bb256e177b097aa24c0
Mehmet Bajin <bajin.mehmet@gmail.com>
2020-06-15 21:37:11 -0400

    feat: ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -6,29 +6,32 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Apollo from './components/Apollo';
 import App from './components/App';
 import Auth from './components/Auth';
+import ErrorBoundary from './components/ErrorBoundary';
 import Firebase from './components/Firebase';
 import Notification from './components/Notification';
 import Sentry from './components/Sentry';
 import Theme from './components/Theme';
 
 render(
-  <CssBaseline>
-    <Theme>
-      <Apollo>
-        <Firebase>
-          <Auth>
-            <Notification>
-              <Helmet
-                titleTemplate="%s | OMSCentral"
-                defaultTitle="OMSCentral"
-              />
-              <App />
-              <Sentry />
-            </Notification>
-          </Auth>
-        </Firebase>
-      </Apollo>
-    </Theme>
-  </CssBaseline>,
+  <ErrorBoundary>
+    <CssBaseline>
+      <Theme>
+        <Apollo>
+          <Firebase>
+            <Auth>
+              <Notification>
+                <Helmet
+                  titleTemplate="%s | OMSCentral"
+                  defaultTitle="OMSCentral"
+                />
+                <Sentry />
+                <App />
+              </Notification>
+            </Auth>
+          </Firebase>
+        </Apollo>
+      </Theme>
+    </CssBaseline>
+  </ErrorBoundary>,
   document.getElementById('root'),
 );
